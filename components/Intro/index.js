1a22ddce699ee35840a69cab37662a6fd38091ca
Timur Çelikel <me@timurcelikel.com>
2017-04-11 15:08:44 +0200

    Intro: use logo, layout

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import style from './style.module.less';
+import SVGinline from 'components/SVGinline';
+import logo from '!svg-inline-loader!graphics/logo.svg';
 
 function Intro ({ children }) {
-    return (
+    return (        
         <section className={style.container}>
-            { children }
+            <div className={style.inner}>
+                <SVGinline svg={logo} 
+                    className={style.logo} />
+                { children }
+            </div>
         </section>
     )
 
