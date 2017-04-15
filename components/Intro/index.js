d8f12629cc31fbd85d8336382f24d15d843c9499
Timur Çelikel <me@timurcelikel.com>
2017-04-15 13:14:48 +0200

    Intro: add basic fishes

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -2,29 +2,81 @@ import React from 'react';
 import style from './style.module.less';
 import SVGinline from 'components/SVGinline';
 import logo from '!svg-inline-loader!graphics/logo.svg';
-import Kiste from 'components/Kiste'
+import { map } from 'lodash';
+import Kiste from 'components/Kiste';
+import SetInnerHTML from 'components/SetInnerHTML'
+
+import fisch from 'graphics/illustrations/fisch1.png'
+
+const IMG_URL = 'graphics/illustrations/'
 
 export default class Intro extends React.Component {
     constructor(props) {
         super(props);
+
+        this.boundScroll = this.onScroll.bind(this);
+        this.state = {
+            scroll: this.getScrollPosition()
+        };
     }
     render() {
         const { children, fishes } = this.props;
-        console.log('fishes', fishes)
 
         return (
-            <div>
-                <section className={style.container}>
+            <div ref={(c) => this.containerEl = c}
+                    className={style.container}>
+                <section className={style.top}>
                     <div className={style.inner}>
                         <SVGinline svg={logo} 
                             className={style.logo} />
                         { children }
                     </div>
                 </section>
+                { map(fishes, (fish, idx) => {
+                        return (
+                            <Fish fish={fish}
+                                key={idx} />
+                        )
+                    })
+                }
                 <div className={style.kiste}>
                     <Kiste />
                 </div>
             </div>
         )
     }
+
+    onScroll() {
+        const scrollPosition = this.getScrollPosition();
+    }
+
+    getScrollPosition() {
+        return typeof window === 'undefined' ? 0 : window.scrollY;
+    }
+
+    componentDidMount() {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', this.boundScroll);
+        }
+    }
+
+    componentWillUnmount() {
+        if (typeof window !== 'undefined') {
+            window.removeEventListener('resize', this.boundScroll);
+        }
+    }
+}
+
+function Fish ({ fish }) {
+    const url = require('graphics/illustrations/' + fish.image)
+    const fishStyle = { backgroundImage: 'url(' + url + ')' };
+    return (
+        <div className={style.fish}>
+            <div className={style.fish_image} style={fishStyle} />
+            <div className={style.fish_description}>
+                {fish.title}
+                <SetInnerHTML body={fish.body} />
+            </div>
+        </div>
+    )
 }
