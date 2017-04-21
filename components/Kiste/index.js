5a26b179a12d7a440ee54694f5bc79a41a2b720b
Timur Çelikel <me@timurcelikel.com>
2017-04-21 10:30:14 +0200

    Kiste: schraube relative position for more sizing flexibility

--- components/Kiste/index.js
+++ components/Kiste/index.js
@@ -8,7 +8,9 @@ function Kiste ({ links, className }) {
     return (
         <div className={classnames(className, style.container)}>
             <div className={style.kiste}>
-                <div className={style.schraube} />
+                <div className={style.schraube}>
+                    <div className={style.schraube_inner} />
+                </div>
             </div>
         </div>
     )
