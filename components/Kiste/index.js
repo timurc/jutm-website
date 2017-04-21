faa061a008886063ad326cf6cc107d67849b2fc2
Timur Çelikel <me@timurcelikel.com>
2017-04-21 11:55:24 +0200

    Kiste: raise arm

--- components/Kiste/index.js
+++ components/Kiste/index.js
@@ -4,7 +4,7 @@ import style from './style.module.less'
 import classnames from 'classnames'
 import { map } from 'lodash'
 
-function Kiste ({ links, className }) {
+function Kiste ({ links, raiseArm, className }) {
     return (
         <div className={classnames(className, style.container)}>
             <div className={style.kiste}>
@@ -13,12 +13,20 @@ function Kiste ({ links, className }) {
                     <div className={style.schraube_inner} />
                 </div>
                 <div className={classnames(style.arm_container, style.arm_container_l)}>
-                    <div className={classnames(style.arm, style.arm_l)}>
+                    <div className={classnames(
+                                style.arm, 
+                                style.arm_l,
+                                {[style.arm__raise]: raiseArm === 'L'}
+                            )}>
                         <div className={classnames(style.arm_l_lower, style.arm_lower)} />
                     </div>
                 </div>
                 <div className={classnames(style.arm_container, style.arm_container_r)}>
-                    <div className={classnames(style.arm, style.arm_r)}>
+                    <div className={classnames(
+                                style.arm, 
+                                style.arm_r,
+                                {[style.arm__raise]: raiseArm === 'R'}
+                            )}>
                         <div className={classnames(style.arm_l_lower, style.arm_lower)} />
                     </div>
                 </div>
