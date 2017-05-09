e2f1076e4b6bfcf0f842bf749479785e7af7d0b3
Timur Çelikel <me@timurcelikel.com>
2017-05-09 16:44:13 +0200

    Index: add tour dates

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,18 @@ import BackgroundMagic from 'components/BackgroundMagic'
 import Intro from 'components/Intro'
 import SetInnerHTML from 'components/SetInnerHTML'
 import Section from 'components/Section'
+import Tour from 'components/Tour'
 import theaterstuck from './_texte/theaterstuck.md'
-import { filter, map } from 'lodash'
+import { filter, map, find } from 'lodash'
 import style from './landing.module.less'
+import frontMatter from 'front-matter'
 
 export default function Index({ route }) {
     const fishes = map(filter(route.pages, (page) => {
         return page.path.indexOf('/_fische') === 0;
     }), (page) => page.data);
+
+    const dates = find(route.pages, {path: '/dates/'})
 
     return (
         <div>
@@ -31,6 +35,10 @@ export default function Index({ route }) {
             <span id="beschreibung" />
             <Section className={style.theaterstuck}title={theaterstuck.title}>
                 <SetInnerHTML body={theaterstuck.body} />
+            </Section>
+            <span id="tour" />
+            <Section title="Tour">
+                <Tour dates={dates.data.dates} />
             </Section>
         </div>
     )
