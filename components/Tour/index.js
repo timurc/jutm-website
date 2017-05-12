9f01edb5c6ac03dbb931e854d998cecc1ce60ddc
Timur Çelikel <me@timurcelikel.com>
2017-05-12 14:37:37 +0200

    Tour: add all meta data to Tour dates

--- components/Tour/index.js
+++ components/Tour/index.js
@@ -1,18 +1,49 @@
 import React from 'react'
 import style from './style.module.less'
 import { map } from 'lodash'
+import moment from 'moment'
 
 function Tour ({ dates }) {
     return (
         <ul className={style.list}>
-            { map(dates, (date, idx) => <Date key={idx} date={date} /> )}
+            { map(dates, (date, idx) => <Event key={idx} date={date} /> )}
         </ul>
     )
 }
 
-function Date ({ date }) {
+function Event ({ date }) {
+    const { venue, city, street, zipCode, link } = date;
+    const dates = map(date.dates, (d) => moment(d));
+
     return (
-        <li>{date.venue}</li>
+        <li className={style.event}>
+            <div className={style.city}>{city}</div>
+            <h2 className={style.venue}>
+                { link ? 
+                    <a href={link} target="_blank">{venue}</a> :
+                    <span>{venue}</span>
+                }
+            </h2>
+            <div className={style.adress}>
+                {street}<br />
+                {zipCode} {city}
+            </div>
+            <ul className={style.dates}>
+                { map(dates, (d, idx) => {
+                        return <Date key={idx} date={d} />
+                    }
+                )}
+            </ul>
+        </li>
+    )
+}
+
+function Date ({ date }) {
+    const formattedDate = date.format('DD.MM.YYYY, HH:mm [Uhr]')
+    return (
+        <li className={style.date}>
+            { formattedDate }
+        </li>
     )
 }
 
