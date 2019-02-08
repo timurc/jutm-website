6dc977e64cbd8b118841f06a34076da7014f05cd
Timur Çelikel <me@timurcelikel.com>
2019-02-08 08:27:15 -0400

    remove piwik tracking

--- html.js
+++ html.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Helmet from "react-helmet"
-import { prefixLink } from 'gatsby-helpers'
-require('file?name=.htaccess!./.htaccess')
+import React from 'react';
+import Helmet from 'react-helmet';
+import { prefixLink } from 'gatsby-helpers';
+require('file?name=.htaccess!./.htaccess');
 
-const BUILD_TIME = new Date().getTime()
+const BUILD_TIME = new Date().getTime();
 
 const propTypes = {
-    body: React.PropTypes.string
-}
+    body: React.PropTypes.string,
+};
 
-export default function HTML ({ body }) {
+export default function HTML({ body }) {
     const head = Helmet.rewind();
     const piwikSetup = buildPiwikSetup();
 
-    let css
+    let css;
     if (process.env.NODE_ENV === 'production') {
-        css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
+        css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />;
     }
 
     return (
@@ -24,21 +24,18 @@ export default function HTML ({ body }) {
                 <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <link rel="shortcut icon" href="/favicon.png" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 {head.title.toComponent()}
                 {head.meta.toComponent()}
                 {css}
-                { process.env.NODE_ENV === 'production' ? piwikSetup : null }
+                {process.env.NODE_ENV === 'production' ? piwikSetup : null}
             </head>
             <body>
                 <div id="react-mount" dangerouslySetInnerHTML={{ __html: body }} />
-                <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
+                {/* <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} /> */}
             </body>
         </html>
-    )
+    );
 }
 
 function buildPiwikSetup() {
@@ -56,10 +53,7 @@ function buildPiwikSetup() {
         })();
     `;
 
-    return <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{ __html: js }}
-        />;
+    return <script type="text/javascript" dangerouslySetInnerHTML={{ __html: js }} />;
 }
 
 HTML.propTypes = propTypes;
